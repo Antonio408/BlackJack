0109aae3c83c90ba9cc4caaa245cc46101477f25
antonio <antonio.ramos2@yahoo.com>
2017-02-08 22:58:54 -0600

    add intent to open mainactivity and about

--- app/src/main/java/com/antonioramos/blackjack/title.java
+++ app/src/main/java/com/antonioramos/blackjack/title.java
@@ -2,6 +2,9 @@ package com.antonioramos.blackjack;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 
 /**
@@ -15,5 +18,23 @@ public class title extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.title);
 
+       Button b = (Button) findViewById(R.id.play_button);
+        b.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        b = (Button) findViewById(R.id.about_button);
+        b.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
